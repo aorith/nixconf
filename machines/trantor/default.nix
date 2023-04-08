93364e97810965556e11f4962405d8c345b49352
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-04-08 16:43:24 +0200

    update

--- machines/trantor/default.nix
+++ machines/trantor/default.nix
@@ -16,6 +16,7 @@ in {
   ];
 
   hardware.pulseaudio.enable = lib.mkForce false;
+  hardware.pulseaudio.extraConfig = "unload-module module-suspend-on-idle";
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -33,29 +34,6 @@ in {
     unstable.pavucontrol
     unstable.helvum # for pipewire
   ];
-
-  # from /run/current-system/sw/share/wireplumber/main.lua.d/50-alsa-config.lua
-  # disable sound suspend to avoid missing sounds on idle or pop noises
-  environment.etc."wireplumber/main.lua.d/51-alsa-custom.lua".text = ''
-    alsa_monitor.rules = {
-      {
-        matches = {
-          {
-            -- Matches all sources.
-            { "node.name", "matches", "alsa_input.*" },
-          },
-          {
-            -- Matches all sinks.
-            { "node.name", "matches", "alsa_output.*" },
-          },
-        },
-        apply_properties = {
-          ["session.suspend-timeout-seconds"] = 0, -- 0 disables suspend
-        },
-      },
-    }
-
-  '';
 
   services = {
     syncthing = {
