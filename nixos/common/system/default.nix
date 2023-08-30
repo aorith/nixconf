b91ea2c42f07824b373cbaaa1814cb19d361ad22
Manuel Sanchez Pinar <aomanu@gmail.com>
2023-08-30 09:40:27 +0200

    update

--- nixos/common/system/default.nix
+++ nixos/common/system/default.nix
@@ -10,12 +10,6 @@
   time.timeZone = "Europe/Madrid";
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_MEASUREMENT = "es_ES.UTF-8";
-      LC_MONETARY = "es_ES.UTF-8";
-      LC_NUMERIC = "es_ES.UTF-8";
-      LC_TIME = "es_ES.UTF-8";
-    };
   };
 
   location.provider = "geoclue2";
