ab22c0f96765959b12968e7734eec75a5ee99032
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-02-29 09:25:55 +0100

    update arcadia

--- hosts/arcadia/fail2ban.nix
+++ hosts/arcadia/fail2ban.nix
@@ -44,8 +44,8 @@
     # Define an action that will trigger a Ntfy push notification upon the issue of every new ban
     "fail2ban/action.d/ntfy.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
       [Definition]
-      norestored = true # Needed to avoid receiving a new notification after every restart
-      actionban = ${pkgs.curl}/bin/curl -H "Title: <ip> has been banned in <name>" -d "<name> jail has banned <ip> from accessing $(hostname) after <failures> attempts." https://ntfy.sh/fail2ban-aorith
+      norestored = true
+      actionban = ${pkgs.curl}/bin/curl -H "Title: [<name>] <ip> has been banned" -d "<name> jail has banned <ip> from accessing $(${pkgs.hostname}/bin/hostname) after <failures> attempts." https://ntfy.sh/fail2ban-aorith
     '');
     # Filter for notes access
     "fail2ban/filter.d/caddy-notes.local".text = pkgs.lib.mkDefault (pkgs.lib.mkAfter ''
