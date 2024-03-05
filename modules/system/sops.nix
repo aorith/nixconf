e92ba023ffc84b4a3e7fecbde83a0a907cdcae1b
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-03-05 17:56:22 +0100

    update

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -6,6 +6,7 @@
   config = lib.mkIf config.custom.system.sops {
     sops.age.keyFile = "/home/aorith/.config/sops/age/keys.txt";
     sops.age.generateKey = false;
+    sops.defaultSopsFile = ./../../secrets/secrets.yaml;
 
     sops.secrets.root-hashedPassword = {
       sopsFile = ./../../secrets/users.yaml;
