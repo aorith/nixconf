5a211d7a6c103c7964c959955f745ad120800535
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-03-13 12:18:57 +0100

    fonts

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -2,19 +2,28 @@
   fonts = {
     enableDefaultPackages = false; # installs extra fonts
     packages = with pkgs; [
-      fira-code
-      fira-code-symbols
-      hack-font
-      iosevka
-      jetbrains-mono
+      liberation_ttf # metric compatible with Arial, Times New Roman and Courier New
+
+      source-code-pro # monospace
+      source-sans-pro # sans-serif
+      source-serif-pro # serif
+
+      # Wide language coverage
       noto-fonts
       noto-fonts-cjk
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
+
+      fira-code
+      fira-code-symbols
+      material-design-icons
       noto-fonts-emoji
       ubuntu_font_family
 
-      material-design-icons
+      hack-font
+      iosevka
+      jetbrains-mono
+
       (nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     ];
   };
