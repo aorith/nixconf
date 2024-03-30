1cfe0ea8fb7da1131f7d2415d8657ac732c5dccd
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-03-30 18:55:29 +0100

    generate unique btrfs mount points

--- modules/nixos/core/btrfs.nix
+++ modules/nixos/core/btrfs.nix
@@ -3,10 +3,13 @@
   lib,
   ...
 }: let
-  btrfsList = builtins.attrValues (lib.filterAttrs (name: fs: fs.fsType == "btrfs") config.fileSystems);
+  # [ { "/dev/disk/by-uuid/fd3f9eb4-5fa6-4f63-99ac-0dce636f4134" = "/"; } ... ]
+  btrfsDevices = lib.mapAttrsToList (name: fs: {"${fs.device}" = fs.mountPoint;}) (lib.filterAttrs (name: fs: fs.fsType == "btrfs") config.fileSystems);
+  # [ "/mnt/storage/tank" "/" ]
+  btrfsList = lib.attrValues (lib.attrsets.mergeAttrsList (lib.lists.reverseList btrfsDevices));
+
   hasBtrfs = builtins.length btrfsList > 0;
 in {
   services.btrfs.autoScrub.enable = hasBtrfs;
-  # The default already maps all the btrfs fs.mountPoints
-  #services.btrfs.autoScrub.fileSystems
+  services.btrfs.autoScrub.fileSystems = btrfsList;
 }
