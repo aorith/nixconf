216ac9e65e2370f822bfed9cc0e7d2c63407ccc5
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-04-27 15:20:35 +0200

    add bash aliases

--- modules/hm/programs.nix
+++ modules/hm/programs.nix
@@ -1,9 +1,18 @@
-{unstable-pkgs, ...}: {
+{pkgs, ...}: {
   programs.fzf.enable = true;
   programs.bash = {
     enable = true;
     initExtra = ''
       . ~/githome/dotfiles/topics/shell/src/bash/bashrc
     '';
+
+    shellAliases = {
+      l = "${pkgs.eza}/bin/eza -lah --git --git-repos --icons=auto";
+      la = "${pkgs.eza}/bin/eza -lah --git --git-repos --icons=auto";
+      ll = "${pkgs.eza}/bin/eza -lh --git --git-repos --icons=auto";
+      #ls = "${pkgs.eza}/bin/eza"; # some flags like '-t' do not behave like ls
+
+      cat = "${pkgs.bat}/bin/bat --plain --theme='Monokai Extended'";
+    };
   };
 }
