aa5361ff9f45ad36cb5a79c52c3a8f8d261fb171
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-05-31 18:37:23 +0200

    tmux: search fixed?

--- overlays/tmux/default.nix
+++ overlays/tmux/default.nix
@@ -1,12 +1,16 @@
 # Pin tmux version, 3.4 search is broken atm
 final: prev: {
   tmux = prev.tmux.overrideAttrs (oldAttrs: rec {
-    version = "3.3a";
+    # version = "3.3a";
+    version = "3.4";
     src = prev.fetchFromGitHub {
       owner = "tmux";
       repo = "tmux";
-      rev = version;
-      sha256 = "sha256-SygHxTe7N4y7SdzKixPFQvqRRL57Fm8zWYHfTpW+yVY=";
+      # rev = version;
+      rev = "c07e856d244d07ab2b65e72328fb9fe20747794b";
+      sha256 = "sha256-99hdAskEByqD4fjl2wrth9QfSkPXkN7o2A9e+BOH6ug=";
     };
+
+    patches = null;
   });
 }
