da76d8bf19aff4a42e5510b0e36939a2bf5db46f
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-09-07 13:40:23 +0200

    add static html for bree

--- hosts/arcadia/caddy/default.nix
+++ hosts/arcadia/caddy/default.nix
@@ -1,6 +1,15 @@
+{ pkgs, ... }:
+let
+  bree-index = pkgs.writeTextDir "bree/www/index.html" (builtins.readFile ./bree.html);
+  caddyfile = pkgs.writeText "caddyfile" (
+    builtins.replaceStrings [ "%%bree_root%%" ] [ "${bree-index.outPath}/bree/www" ] (
+      builtins.readFile ./caddyfile
+    )
+  );
+in
 {
   services.caddy = {
     enable = true;
-    configFile = ./caddyfile;
+    configFile = caddyfile.outPath;
   };
 }
