d6aa511ae9693a4c4b81cfb27b513a319a4e406b
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-11-15 14:45:58 +0100

    update silverbullet

--- hosts/arcadia/silverbullet.nix
+++ hosts/arcadia/silverbullet.nix
@@ -1,23 +1,24 @@
 { pkgs, unstable-pkgs, ... }:
 {
-  # imports = [
-  #   # 24.05 is already here
-  #   # "${inputs.nixpkgs-unstable}/nixos/modules/services/web-apps/silverbullet.nix"
-  # ];
-
   config = {
     services.silverbullet = {
       enable = true;
       package = unstable-pkgs.silverbullet.overrideAttrs (oldAttrs: rec {
-        version = "0.9.4";
+        version = "0.10.1";
         src = pkgs.fetchurl {
           url = "https://github.com/silverbulletmd/silverbullet/releases/download/${version}/silverbullet.js";
-          hash = "sha256-J0fy1e/ObpujBNSRKA55oU30kXNfus+5P2ebggEN6Dw=";
+          hash = "sha256-4ZnA5cmLDlEUpeTBgz6Wg3XK3JJpgdt9bf9Eg7o82T8=";
         };
       });
       listenPort = 3000;
       listenAddress = "127.0.0.1";
       envFile = "/etc/silverbullet.env";
+    };
+
+    # https://github.com/NixOS/nixpkgs/pull/356163
+    systemd.services.silverbullet.serviceConfig = {
+      Environment = "DENO_DIR=/var/cache/silverbullet/deno";
+      CacheDirectory = "silverbullet";
     };
 
     # For the git plugin
