77b8e5b5ac2ab3da2822b112eae18707163abb9b
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-09-15 13:34:14 +0200

    update

--- hosts/arcadia/silverbullet.nix
+++ hosts/arcadia/silverbullet.nix
@@ -3,23 +3,23 @@
   config = {
     services.silverbullet = {
       enable = true;
-      package = unstable-pkgs.silverbullet.overrideAttrs (oldAttrs: rec {
-        version = "0.10.4";
-        src = pkgs.fetchurl {
-          url = "https://github.com/silverbulletmd/silverbullet/releases/download/${version}/silverbullet.js";
-          hash = "sha256-ko1zXfvn0rVY+lp9zTZ71BL41h7AOazooBVeqELP3Ps=";
-        };
-      });
+      #  package = unstable-pkgs.silverbullet.overrideAttrs (oldAttrs: rec {
+      #    version = "0.10.4";
+      #    src = pkgs.fetchurl {
+      #      url = "https://github.com/silverbulletmd/silverbullet/releases/download/${version}/silverbullet.js";
+      #      hash = "sha256-ko1zXfvn0rVY+lp9zTZ71BL41h7AOazooBVeqELP3Ps=";
+      #    };
+      #  });
       listenPort = 3000;
       listenAddress = "10.255.254.1";
       envFile = "/etc/silverbullet.env";
     };
 
     # https://github.com/NixOS/nixpkgs/pull/356163
-    systemd.services.silverbullet.serviceConfig = {
-      Environment = "DENO_DIR=/var/cache/silverbullet/deno";
-      CacheDirectory = "silverbullet";
-    };
+    #systemd.services.silverbullet.serviceConfig = {
+    #  Environment = "DENO_DIR=/var/cache/silverbullet/deno";
+    #  CacheDirectory = "silverbullet";
+    #};
 
     # For the git plugin:
     #   Get a (beta) token scoped for this repo: https://github.com/settings/tokens
