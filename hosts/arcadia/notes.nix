6f3273e653cbd45522787a22889f8389081e6d17
Manuel Sanchez Pinar <aomanu@gmail.com>
2025-10-02 12:34:28 +0200

    generate notes using github.com/DannyBen/madness

--- hosts/arcadia/notes.nix
+++ hosts/arcadia/notes.nix
@@ -1,29 +1,16 @@
-{ pkgs, ... }:
-let
-  static_notes_script = pkgs.writeScriptBin "generate-static-notes.py" ''
-    #!${pkgs.python3}/bin/python
-    ${builtins.readFile ./generate-static-notes.py}
-  '';
-in
 {
-  systemd.services."notes-static" = {
-    description = "Static Notes";
-    wantedBy = [ "multi-user.target" ];
-    path = with pkgs; [
-      quarto
-      strip-ansi
-    ];
-    serviceConfig = {
-      User = "root";
-      Group = "root";
-      ExecStart = "${static_notes_script}/bin/generate-static-notes.py /var/lib/syncthing/SYNC_STUFF/notes/zk/notes /tmp/notes";
-      Restart = "on-failure";
-      Environment = [
-        "NO_COLOR=1"
-        "PYTHON_UNBUFFERED=1"
-        "HOME=/tmp/.quarto"
-        "XDG_CACHE_HOME=/tmp/.quarto/cache"
-      ];
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      notes = {
+        autoStart = true;
+        image = "dannyben/madness:1.2.4";
+        ports = [
+          "10.255.254.1:8323:3000"
+        ];
+        volumes = [ "/var/lib/syncthing/SYNC_STUFF/notes/zk/notes:/docs" ];
+        cmd = [ "server" ];
+      };
     };
   };
 }
