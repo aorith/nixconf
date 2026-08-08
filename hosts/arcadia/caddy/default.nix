9ffa2493b6517cd323aa804a34017ab392487856
Manuel Sanchez Pinar <aomanu@gmail.com>
2026-08-08 16:24:45 +0200

    Serve domain with wildcard

--- hosts/arcadia/caddy/default.nix
+++ hosts/arcadia/caddy/default.nix
@@ -1,8 +1,35 @@
-{ pkgs, unstable-pkgs, ... }:
 {
+  pkgs,
+  config,
+  unstable-pkgs,
+  ...
+}:
+let
+  caddyfile = pkgs.replaceVars ./Caddyfile {
+    CERT_PATH = "${config.security.acme.certs."iou.re".directory}/cert.pem";
+    KEY_PATH = "${config.security.acme.certs."iou.re".directory}/key.pem";
+  };
+in
+{
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "aomanu+acme@gmail.com";
+
+    certs."iou.re" = {
+      domain = "*.iou.re";
+      dnsProvider = "gandiv5";
+      environmentFile = "/etc/gandi-api-token.env";
+      group = "caddy";
+      reloadServices = [ "caddy.service" ];
+    };
+  };
+
+  users.users.caddy.extraGroups = [ "acme" ];
+
   services.caddy = {
     enable = true;
-    configFile = ./Caddyfile;
+    configFile = caddyfile;
     package = unstable-pkgs.caddy;
   };
 }
