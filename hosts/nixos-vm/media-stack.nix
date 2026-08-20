5b76e875bd27f36f8657662ac97804fe6a2e4457
Manuel Sanchez Pinar <aomanu@gmail.com>
2026-08-20 19:18:41 +0200

    update

--- hosts/nixos-vm/media-stack.nix
+++ hosts/nixos-vm/media-stack.nix
@@ -86,25 +86,27 @@ in
     # I2P (Invisible Internet Project) is an anonymous, decentralized overlay network.
     # It routes traffic inside its own encrypted network of peers
     i2pd = {
-      enable = true;
-      port = 10100; # UDP - open it optionally
-      proto = {
+      enable = false;
+
+      settings = {
+        port = 10100; # UDP - open it optionally
+
         http = {
           # Web interface
-          enable = true;
+          enabled = true;
           address = "10.255.255.8";
           port = 10101;
         };
-        httpProxy = {
+        httpproxy = {
           # Test: curl "http://notbob.i2p" --proxy 10.255.255.8:10102
-          enable = true;
+          enabled = true;
           address = "10.255.255.8";
           port = 10102;
           # NOTE: outproxy: Address of a proxy server inside I2P, which is used to visit regular Internet
           # outproxy = "http://false.i2p";
         };
         sam = {
-          enable = true;
+          enabled = true;
           port = 10103;
         };
       };
